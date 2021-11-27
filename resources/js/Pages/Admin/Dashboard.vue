623a16e7073fd1a774f06d7ee0e9890c3a076e78
MeesPos <meespostma_@hotmail.com>
2021-11-27 18:58:32 +0100

    Added Dashboard layout

--- resources/js/Pages/Admin/Dashboard.vue
+++ resources/js/Pages/Admin/Dashboard.vue
@@ -1,9 +1,15 @@
 <template>
+    <HeaderTitle title="Dashboard" />
     Landed on Dashboard
 </template>
 
 <script>
+import Admin from '../../Layouts/Admin'
+import HeaderTitle from "../../Shared/Dashboard/HeaderTitle";
+
 export default {
-    name: "Dashboard"
+    name: "Dashboard",
+    components: {HeaderTitle},
+    layout: Admin
 }
 </script>
---
+++ resources/js/Shared/MobileSidebar.vue
@@ -0,0 +1,27 @@
+<template>
+    <nav class="px-2 space-y-1">
+        <Link v-for="item in navigation"
+              :key="item.name"
+              :href="item.href"
+              :class="[item.current ? 'bg-gray-900 text-white' : 'text-gray-300 hover:bg-gray-700 hover:text-white', 'group flex items-center px-2 py-2 text-base font-medium rounded-md']">
+            <component :is="item.icon"
+                       :class="[item.current ? 'text-gray-300' : 'text-gray-400 group-hover:text-gray-300', 'mr-4 flex-shrink-0 h-6 w-6']"
+                       aria-hidden="true"
+            />
+            {{ item.name }}
+        </Link>
+    </nav>
+</template>
+
+<script>
+export default {
+    name: "MobileSidebar",
+    props: {
+        navigation: Array
+    }
+}
+</script>
+
+<style scoped>
+
+</style>
