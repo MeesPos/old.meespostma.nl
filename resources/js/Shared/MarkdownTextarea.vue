89dd091b3c09ca23eb18e6c5991ddc2464ec209d
MeesPos <meespostma_@hotmail.com>
2021-12-03 22:24:14 +0100

    Completed markdown wysiwyg textarea

--- resources/js/Shared/MarkdownTextarea.vue
+++ resources/js/Shared/MarkdownTextarea.vue
@@ -8,12 +8,18 @@ import {Quill, QuillEditor} from '@vueup/vue-quill'
 import MarkdownShortcuts from 'quill-markdown-shortcuts'
 import BlotFormatter from 'quill-blot-formatter'
 import QuillEmoji from 'quill-emoji'
+import HtmlEditButton from 'quill-html-edit-button'
+import { ImageDrop } from 'quill-image-drop-module'
+import MagicUrl from 'quill-magic-url'
 import '@vueup/vue-quill/dist/vue-quill.snow.css';
 import "quill-emoji/dist/quill-emoji.css";
 
 Quill.register("modules/MarkdownShortcuts", MarkdownShortcuts);
 Quill.register("modules/BlotFormatter", BlotFormatter);
 Quill.register("modules/QuillEmoji", QuillEmoji);
+Quill.register("modules/HtmlEditButton", HtmlEditButton);
+Quill.register("modules/ImageDrop", ImageDrop);
+Quill.register("modules/MagicUrl", MagicUrl);
 
 export default defineComponent({
     name: "MarkdownTextarea",
@@ -52,6 +58,18 @@ export default defineComponent({
                     BlotFormatter: {
                         name: 'blotFormatter',
                         module: BlotFormatter
+                    },
+                    HtmlEditButton: {
+                        name: "HtmlEditButton",
+                        module: HtmlEditButton
+                    },
+                    ImageDrop: {
+                        name: "ImageDrop",
+                        module: ImageDrop
+                    },
+                    MagicUrl: {
+                        name: "MagicUrl",
+                        module: MagicUrl
                     },
                     "emoji-toolbar": true,
                     "emoji-shortname": true,
