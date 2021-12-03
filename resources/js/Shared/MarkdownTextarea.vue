f5a479ec870252d55e5a3b5708b5ca5d30051bd2
MeesPos <meespostma_@hotmail.com>
2021-12-03 22:10:12 +0100

    Added image resize package and emoji package

--- resources/js/Shared/MarkdownTextarea.vue
+++ resources/js/Shared/MarkdownTextarea.vue
@@ -1,12 +1,19 @@
 <template>
-    <QuillEditor :modules="modules" theme="snow" :toolbar="toolbar" />
+    <QuillEditor :options="quillConfig" />
 </template>
 
 <script>
-import { defineComponent } from "vue";
-import { QuillEditor } from '@vueup/vue-quill'
+import {defineComponent} from "vue";
+import {Quill, QuillEditor} from '@vueup/vue-quill'
 import MarkdownShortcuts from 'quill-markdown-shortcuts'
+import BlotFormatter from 'quill-blot-formatter'
+import QuillEmoji from 'quill-emoji'
 import '@vueup/vue-quill/dist/vue-quill.snow.css';
+import "quill-emoji/dist/quill-emoji.css";
+
+Quill.register("modules/MarkdownShortcuts", MarkdownShortcuts);
+Quill.register("modules/BlotFormatter", BlotFormatter);
+Quill.register("modules/QuillEmoji", QuillEmoji);
 
 export default defineComponent({
     name: "MarkdownTextarea",
@@ -15,35 +22,43 @@ export default defineComponent({
     },
     data() {
         return {
-            toolbar: [ [
-                    'bold',
-                    'italic',
-                    'underline',
-                    'strike',
-                ], [
-                    'blockquote',
-                    'code-block',
-                ], [
-                    { 'list': 'ordered' },
-                    { 'list': 'bullet' },
-                    { 'align': ['', 'center', 'right'] },
-                ], [
-                    { 'header': ['1', '2', '3', '4', '5', '6', 'normal'] },
-                ], [
-                    'link',
-                    'video',
-                    'image'
-                ]
-            ]
-        }
-    },
-    setup: () => {
-        const modules = {
-            name: 'markdownShortcuts',
-            module: MarkdownShortcuts
-        }
-        return { modules }
-    },
+            quillConfig: {
+                theme: "snow",
+                modules: {
+                    toolbar: [[
+                        'bold',
+                        'italic',
+                        'underline',
+                        'strike',
+                    ], [
+                        'blockquote',
+                        'code-block',
+                    ], [
+                        {'list': 'ordered'},
+                        {'list': 'bullet'},
+                        {'align': ['', 'center', 'right']},
+                    ], [
+                        {'header': ['1', '2', '3', '4', '5', '6', 'normal']},
+                    ], [
+                        'link',
+                        'video',
+                        'image',
+                        'emoji'
+                    ]],
+                    MarkdownShortcuts: {
+                        name: 'markdownShortcuts',
+                        module: MarkdownShortcuts
+                    },
+                    BlotFormatter: {
+                        name: 'blotFormatter',
+                        module: BlotFormatter
+                    },
+                    "emoji-toolbar": true,
+                    "emoji-shortname": true,
+                }
+            },
 
+        }
+    }
 })
 </script>
