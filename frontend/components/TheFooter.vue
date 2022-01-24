848d5d79ac979557ea9bb7499ba7cad87c1d4ece
MeesPos <meespostma_@hotmail.com>
2022-01-24 22:49:01 +0100

    Setted up start for Nuxt 3

---
+++ frontend/components/TheFooter.vue
@@ -0,0 +1,5 @@
+<template>
+  <footer>
+    Copyright &copy; 2022 <a href="https://www.meespostma.nl">meespostma.nl</a>
+  </footer>
+</template>
---
+++ frontend/layouts/default.vue
@@ -0,0 +1,26 @@
+<template>
+  <Html>
+    <Head>
+      <Title>Welcome to meespostma.nl</Title>
+    </Head>
+  </Html>
+
+  <TheHeader />
+
+  <slot />
+
+  <TheFooter />
+</template>
+
+<script>
+import TheHeader from "../components/TheHeader";
+import TheFooter from "../components/TheFooter";
+export default {
+  name: "default",
+  components: {TheFooter, TheHeader}
+}
+</script>
+
+<style scoped>
+
+</style>
