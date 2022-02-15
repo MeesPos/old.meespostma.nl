919022b0d0582cfee1e9ecc887a92e0541757085
MeesPos <meespostma_@hotmail.com>
2022-02-15 21:40:19 +0100

    Fixed translations

--- resources/js/Pages/Home.vue
+++ resources/js/Pages/Home.vue
@@ -54,31 +54,31 @@
                             <h1 class="text-4xl leading-tight font-bold text-gray-900 xl:text-5xl">{{ this.$t('home.header.title') }}</h1>
                             <h2 class="text-2xl leading-tight font-medium text-gray-900 xl:text-3xl mt-2">{{ this.$t('home.header.subtitle') }}</h2>
                         </div>
-                        <p class="max-w-lg text-gray-600 md:max-w-none">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Delectus, hic veritatis? Debitis fuga quisquam tenetur voluptatum. Accusantium aliquid dolor facere incidunt maiores odio, omnis quam quo similique veritatis! Ipsum, quam.</p>
-                        <a href="#_" class="w-full px-12 py-5 text-sm font-medium text-center text-white uppercase bg-gray-900 rounded-full sm:w-auto">About me</a>
+                        <p class="max-w-lg text-gray-600 md:max-w-none">{{ this.$t('home.header.description') }}</p>
+                        <a href="#_" class="w-full px-12 py-5 text-sm font-medium text-center text-white uppercase bg-gray-900 rounded-full sm:w-auto">{{ this.$t('about-me') }}</a>
                     </div>
                 </div>
                 <!-- start bottom cards -->
                 <div class="flex flex-col w-full lg:flex-row lg:space-x-10 lg:mt-10 sm:pb-10">
-                    <div class="flex flex-col items-center justify-between w-full p-0 overflow-hidden bg-yellow-100 lg:rounded-2xl sm:flex-row lg:justify-start sm:p-10">
+                    <div class="flex flex-col items-center justify-between w-full p-0 overflow-hidden bg-blue-100 lg:rounded-2xl sm:flex-row sm:p-10">
                         <div class="relative flex items-center p-8 sm:p-0">
                             <img src="/images/project-icon.png" class="w-20 mr-5">
                             <div class="relative pr-5">
-                                <h2 class="font-medium text-yellow-800">Get Started Today</h2>
-                                <p class="mt-1 text-sm text-yellow-600">Start crafting the page of your dreams</p>
+                                <h2 class="font-medium text-blue-800">{{ this.$t('my-projects') }}</h2>
+                                <p class="mt-1 text-sm text-blue-600">{{ this.$t('home.my-projects.description') }}</p>
                             </div>
                         </div>
-                        <a href="#_" class="inline-flex justify-center flex-shrink-0 w-full px-5 py-3 mt-2 text-xs font-medium text-center text-white uppercase bg-yellow-400 sm:rounded-full sm:w-auto">Join for Free</a>
+                        <a href="#_" class="inline-flex justify-center flex-shrink-0 w-full px-5 py-3 mt-2 text-xs font-medium text-center text-white uppercase bg-blue-400 sm:rounded-full sm:w-auto">{{ this.$t('my-projects') }}</a>
                     </div>
-                    <div class="flex flex-col items-center justify-between w-full p-0 overflow-hidden bg-green-100 lg:rounded-2xl sm:flex-row lg:justify-start sm:p-10">
+                    <div class="flex flex-col items-center justify-between w-full p-0 overflow-hidden bg-green-100 lg:rounded-2xl sm:flex-row  sm:p-10">
                         <div class="relative flex items-center p-8 sm:p-0">
                             <img src="/images/contact-icon.png" class="w-20 mr-5">
                             <div class="relative pr-5">
-                                <h2 class="font-medium text-green-800">Everyone Loves Candy</h2>
-                                <p class="mt-1 text-sm text-green-600">It's eye-candy to delight your users</p>
+                                <h2 class="font-medium text-green-800">{{ this.$t('contact-me') }}</h2>
+                                <p class="mt-1 text-sm text-green-600">{{ this.$t('home.contact-me.description') }}</p>
                             </div>
                         </div>
-                        <a href="#_" class="inline-flex justify-center flex-shrink-0 w-full px-5 py-3 mt-2 text-xs font-medium text-center text-white uppercase bg-green-400 sm:rounded-full sm:w-auto">Join for Free</a>
+                        <a href="#_" class="inline-flex justify-center flex-shrink-0 w-full px-5 py-3 mt-2 text-xs font-medium text-center text-white uppercase bg-green-400 sm:rounded-full sm:w-auto">{{ this.$t('contact-me') }}</a>
                     </div>
                 </div>
             </div>
