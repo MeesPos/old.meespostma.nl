13695d9aabf5a5ce3b5f896e133cd8bbbe9dfdea
MeesPos <meespostma_@hotmail.com>
2022-02-17 21:46:45 +0100

    Added project controller, route and inertia view

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,5 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return \Inertia\Inertia::render('Home');
 });
+
+Route::get('projects', [\App\Http\Controllers\ProjectController::class, 'index']);
