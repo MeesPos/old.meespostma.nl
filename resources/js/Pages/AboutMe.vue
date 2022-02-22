e3caa7c2da4007f69e3d04fba6b20735876e60f4
MeesPos <meespostma_@hotmail.com>
2022-02-22 21:22:16 +0100

    Added first content block on about me

--- resources/js/Pages/AboutMe.vue
+++ resources/js/Pages/AboutMe.vue
@@ -1,5 +1,22 @@
 <template>
 
+    <!-- Section 1 -->
+    <section class="w-full bg-white">
+        <div class="flex flex-col px-10 py-20 mx-auto max-w-7xl items-center lg:flex-row">
+            <div class="relative w-full mb-10 lg:text-left sm:text-center lg:w-1/2 xl:w-7/12 lg:mb-0">
+                <h2 class="text-4xl font-bold sm:text-5xl">👋 Hi! I'm Mees Postma.</h2>
+                <p class="text-gray-600 lg:max-w-xl mt-9">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Facilis officia porro ratione recusandae reiciendis soluta unde? A ad culpa dolore dolorum enim, excepturi, incidunt magnam maiores officia, quae quia saepe. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Debitis eos expedita molestias non, praesentium quibusdam quod. Aliquid aspernatur consequuntur cumque dolorum eaque eligendi ipsa labore laboriosam neque, obcaecati quas quos!</p>
+                <button href="#_" class="py-3 px-5 mt-9 text-sm font-medium tracking-widest text-white uppercase bg-gray-900 rounded-lg">
+                    <span class="font-medium">Curriculum Vitae</span>
+                </button>
+            </div>
+
+            <div class="relative flex flex-col items-center justify-center flex-shrink-0 w-full max-w-2xl mx-auto rounded-lg lg:w-1/2 lg:mx-0 xl:w-5/12">
+                <img class="rounded-xl" src="/images/mees-postma.jpeg">
+            </div>
+        </div>
+    </section>
+
 </template>
 
 <script>
--- resources/js/Pages/Home.vue
+++ resources/js/Pages/Home.vue
@@ -4,11 +4,11 @@
             <div class="items-start justify-center py-12 bg-gray-100 lg:rounded-xl px-14 md:py-20 xl:px-20 xl:py-20">
                 <div class="flex flex-col items-start justify-center w-full space-y-10 text-left md:w-3/4 sm:text-center md:text-left sm:items-center md:items-start">
                     <div class="relative">
-                        <h1 class="text-4xl leading-tight font-bold text-gray-900 xl:text-5xl">{{ this.$t('home.header.title') }}</h1>
+                        <h1 class="text-4xl leading-tight font-bold text-gray-900 xl:text-5xl">👋 {{ this.$t('home.header.title') }}</h1>
                         <h2 class="text-2xl leading-tight font-medium text-gray-900 xl:text-3xl mt-2">{{ this.$t('home.header.subtitle') }}</h2>
                     </div>
                     <p class="max-w-lg text-gray-600 md:max-w-none">{{ this.$t('home.header.description') }}</p>
-                    <a href="#_" class="w-full px-12 py-5 text-sm font-medium text-center text-white uppercase bg-gray-900 rounded-full sm:w-auto">{{ this.$t('about-me') }}</a>
+                    <Link href="/about-me" class="w-full px-12 py-5 text-sm font-medium text-center text-white uppercase bg-gray-900 rounded-full sm:w-auto">{{ this.$t('about-me') }}</Link>
                 </div>
             </div>
             <!-- start bottom cards -->
