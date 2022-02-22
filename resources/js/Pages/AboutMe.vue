7047344606414755af877f48e5a2007ea7335f57
MeesPos <meespostma_@hotmail.com>
2022-02-22 22:13:16 +0100

    Added "Who am I?" and "What am I doing?" blocks

--- resources/js/Pages/AboutMe.vue
+++ resources/js/Pages/AboutMe.vue
@@ -1,8 +1,6 @@
 <template>
-
-    <!-- Section 1 -->
-    <section class="w-full bg-white">
-        <div class="flex flex-col px-10 py-20 mx-auto max-w-7xl items-center lg:flex-row">
+    <section class="w-full">
+        <div class="flex flex-col px-10 py-12 mx-auto max-w-7xl items-center lg:flex-row">
             <div class="relative w-full mb-10 lg:text-left sm:text-center lg:w-1/2 xl:w-7/12 lg:mb-0">
                 <h2 class="text-4xl font-bold sm:text-5xl">👋 Hi! I'm Mees Postma.</h2>
                 <p class="text-gray-600 lg:max-w-xl mt-9">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Facilis officia porro ratione recusandae reiciendis soluta unde? A ad culpa dolore dolorum enim, excepturi, incidunt magnam maiores officia, quae quia saepe. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Debitis eos expedita molestias non, praesentium quibusdam quod. Aliquid aspernatur consequuntur cumque dolorum eaque eligendi ipsa labore laboriosam neque, obcaecati quas quos!</p>
@@ -13,6 +11,36 @@
 
             <div class="relative flex flex-col items-center justify-center flex-shrink-0 w-full max-w-2xl mx-auto rounded-lg lg:w-1/2 lg:mx-0 xl:w-5/12">
                 <img class="rounded-xl" src="/images/mees-postma.jpeg">
+            </div>
+        </div>
+    </section>
+
+    <section class="w-full">
+        <div class="flex flex-col px-10 py-12 mx-auto max-w-7xl">
+            <h3 class="text-4xl font-bold sm:text-5xl">Who am I?</h3>
+            <div class="grid grid-cols-6 gap-5 mt-8 md:grid-cols-12 md:gap-10 lg:mt-12">
+                <p class="text-gray-600 col-span-6">
+                    We are innovating and creating the next best interfaces for a beautiful user experience. Build your next interface with our intuitive drag'n drop builder. Take advantage of our tools and features to take your business to the next level!
+                </p>
+
+                <p class="text-gray-600 col-span-6">
+                    Craft an application that you are proud to call your own. Aesthetically pleasing interfaces are now easier than ever to create. Don't get left behind in this era of tools and features designed to help you on your journey.
+                </p>
+            </div>
+        </div>
+    </section>
+
+    <section class="w-full">
+        <div class="flex flex-col px-10 py-12 mx-auto max-w-7xl">
+            <h3 class="text-4xl font-bold sm:text-5xl">What am I doing?</h3>
+            <div class="grid grid-cols-6 gap-5 mt-8 md:grid-cols-12 md:gap-10 lg:mt-12">
+                <p class="text-gray-600 col-span-6">
+                    We are innovating and creating the next best interfaces for a beautiful user experience. Build your next interface with our intuitive drag'n drop builder. Take advantage of our tools and features to take your business to the next level!
+                </p>
+
+                <p class="text-gray-600 col-span-6">
+                    Craft an application that you are proud to call your own. Aesthetically pleasing interfaces are now easier than ever to create. Don't get left behind in this era of tools and features designed to help you on your journey.
+                </p>
             </div>
         </div>
     </section>
