1bdf0373c99d9b819a61db6fc560bf8aec1a77d0
MeesPos <meespostma_@hotmail.com>
2022-02-22 22:27:02 +0100

    Refactored and added right links to footer

--- resources/js/Shared/Footer.vue
+++ resources/js/Shared/Footer.vue
@@ -5,10 +5,21 @@
         <div class="px-10 mx-auto max-w-7xl">
             <div class="flex flex-col justify-between mt-5 mb-5 text-center sm:mb-0 sm:mt-12 sm:flex-row">
                 <ul class="flex flex-row justify-center pb-3 -ml-4 -mr-4 text-sm font-bold">
-                    <li> <a href="#_" class="px-2 text-gray-500 hover:text-gray-600">Contact</a> </li>
-                    <li> <a href="#_" class="px-2 text-gray-500 hover:text-gray-600">About US</a> </li>
-                    <li> <a href="#_" class="px-2 text-gray-500 hover:text-gray-600">FAQ's</a> </li>
-                    <li class=""> <a href="#_" class="px-2 text-gray-500 hover:text-gray-600">Terms</a></li>
+                    <FooterLink href="/">
+                        Home
+                    </FooterLink>
+
+                    <FooterLink href="/about-me">
+                        {{ this.$t('about-me') }}
+                    </FooterLink>
+
+                    <FooterLink href="/projects">
+                        {{ this.$t('my-projects') }}
+                    </FooterLink>
+
+                    <FooterLink href="/contact">
+                        {{ this.$t('contact-me') }}
+                    </FooterLink>
                 </ul>
                 <p class="mt-3 text-xs leading-tight text-gray-500 sm:mt-0"> © Mees Postma {{ new Date().getFullYear() }}</p>
 
@@ -18,7 +29,9 @@
 </template>
 
 <script>
+import FooterLink from "./FooterLink";
 export default {
-    name: "Footer"
+    name: "Footer",
+    components: {FooterLink}
 }
 </script>
---
+++ resources/js/Shared/FooterLink.vue
@@ -0,0 +1,19 @@
+<template>
+    <li>
+        <Link :href="href" class="px-2 text-gray-500 hover:text-gray-600">
+            <slot />
+        </Link>
+    </li>
+</template>
+
+<script>
+export default {
+    name: "FooterLink",
+    props: {
+        href: {
+            type: String,
+            required: true
+        }
+    }
+}
+</script>
